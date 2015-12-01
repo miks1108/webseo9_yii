2645392f522827b662cc159abad602808656c12a
Вадим <guts.vadim@gmail.com>
2015-12-01 21:08:20 +0200

    form action

--- views/notes/list.php
+++ views/notes/list.php
@@ -1,5 +1,16 @@
 <h1>Notes list</h1>
 
+<form action="/notes/new" method="POST">
+    <input class="form-control" type="text" name="title">
+    <br>
+    <textarea class="form-control" name="text"></textarea>
+    <br>
+
+    <input class="btn btn-success" type="submit" value="Add">
+</form>
+
+<br>
+
 <ul>
     <? foreach($notes as $note) { ?>
         <li><?=$note->title?></li>

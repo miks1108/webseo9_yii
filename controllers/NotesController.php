041b9ed7aa2220615cccc531c5dd64ba68712644
Вадим <guts.vadim@gmail.com>
2015-12-01 20:14:18 +0200

    select notes from database with Note ActiveRecord

--- controllers/NotesController.php
+++ controllers/NotesController.php
@@ -1,22 +1,13 @@
 <?php
 namespace app\controllers;
 
+use app\models\Note;
 use yii\web\Controller;
-use yii\db\Connection;
 
 class NotesController extends Controller {
 
     public function actionIndex() {
-
-        $db = new Connection([
-            'dsn' => 'mysql:host=localhost;dbname=yii_db',
-            'username' => 'root',
-            'password' => 'GvSk1{@CYussHfm',
-            'charset' => 'utf8',
-        ]);
-
-        $notes = $db->createCommand("SELECT * FROM notes")->queryAll();
-
+        $notes = Note::findAll();
         return $this->render('list', ['notes' => $notes]);
     }
 

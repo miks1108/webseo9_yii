7e8534c9fbb170010a822bfb6aa703f11021c971
Вадим <guts.vadim@gmail.com>
2015-12-17 19:32:45 +0200

    db config

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=basic',
+    'dsn' => 'mysql:host=localhost;dbname=yii_db',
     'username' => 'root',
-    'password' => '',
+    'password' => 'GvSk1{@CYussHfm',
     'charset' => 'utf8',
 ];
